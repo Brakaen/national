cf5cf2908a5faf8d4ae69c86a78b9dc3a08f72da
Brakaen <brakaenn@gmail.com>
2026-06-30 17:37:17 +0530

    Update ProductDetails.jsx

--- src/infopages/ProductDetails.jsx
+++ src/infopages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import Breadcrumb from "./Breadcrumb";
-import { products } from "./productsdata";
+import { products } from "./Productsdata";
 
 const ProductDetails = () => {
   const { id } = useParams();
